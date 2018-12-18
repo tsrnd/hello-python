6a5f7394d9a9d49a8bae0918a50bafb8d2e7e7d4
ml-tram-pham <tram.pham-fr@asiantech.vn>
2018-12-18 17:28:14 +0700

    variable and data type

---
+++ variabletype.py
@@ -0,0 +1,20 @@
+# define variable
+a = 2  # type int
+b = "ab\nc"  # type string
+c = 1.1  # type float
+d = complex(1, 2)
+e = r'ab\nc'
+
+# print variable
+print(a, b)
+print(b)
+print("phần thực", d.real)
+print("phần ảo", d.imag)
+# check type variable
+print(type(b))
+print(type(a))
+# convert type
+print(str(a))
+print(type(str(a)))
+print(bool('true'))
+print(e)
