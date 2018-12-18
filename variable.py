fd76272e152268e23a5d69cd8c907ac1aa6b1675
at-nhan-nguyen2 <nhan.nguyen2@asiantech.vn>
2018-12-18 15:29:18 +0700

    variable python

---
+++ variable.py
@@ -0,0 +1,18 @@
+# multiple assignment
+a = b = c = 1
+print("a =", a, ", b = ", b, ", c = ", c)
+
+# string
+a = b = "string"
+print("a = %s" % (a), ", b = ", b, ", c = ", c)
+print("a = %s, b = %s, c = %d" % (a, b, c))
+
+# %s - String (or any object with a string representation, like numbers)
+# %d - Integers
+# %f - Floating point numbers
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+# %x/%X - Integers in hex representation (lowercase/uppercase)
+
+# float
+floatA = 7.0
+floatB = float(7)
