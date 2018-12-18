b152470961eae03ba7804512621a69cac6761fe5
at-tamnguyen4 <tam.nguyen4@asiantech.vn>
2018-12-18 15:37:12 +0700

    if else example

---
+++ if-else.py
@@ -0,0 +1,18 @@
+a = 9
+if a > 0:
+    print("Positive number")
+elif a == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+print("=============")
+
+b = -4
+if b >= 0:
+    if b == 0:
+        print("Zero")
+    else:
+        print("Postitive number")
+else:
+    print("Negative number")
