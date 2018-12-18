7a33b1cfa241e86a83161e49fe80b9f31ebe2976
at-minhdao <minh.dao@asiantech.vn>
2018-12-18 14:01:59 +0700

    variable type

---
+++ 1_variable_type.py
@@ -0,0 +1,11 @@
+# multiple assignment
+a = b = c = 1
+print("a =", a, ", b = ", b, ", c = ", c)
+
+a, b, c = 1, 2, "minh"
+print("a =", a, ", b = ", b, ", c = ", c)
+
+# string
+str = "Hello world!"
+print(str)
+print(str[1])
