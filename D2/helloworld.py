52c21b7be19875df144072f9a4d46f2481827af5
at-trangle <trang.le2@asiantech.com>
2018-12-19 09:58:19 +0700

    Uppercase String

---
+++ D2/helloworld.py
@@ -0,0 +1,8 @@
+string = 'hello world'
+uppercase = ['u','e','o','a','i']
+output = ''
+for i in range(len(string)) :
+    if string[i] in uppercase:
+        output += string[i].upper()
+    else : output += string[i]
+print(output)
