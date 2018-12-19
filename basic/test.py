2c1b48d13b513bced99d1aeb43af5bffc13ee4d2
Tuan Dang Q <tuan.dang@asiantech.com.vn>
2018-12-19 17:29:16 +0700

    Finish day 2

--- basic/test.py
+++ basic/test.py
@@ -15,4 +15,26 @@ def mixedFractionToImproper(a):
     
     return mixedFractionToImproper(mixValue);
 
-print(mixedFractionToImproper([3, 1, 2, 4]))
+#print(mixedFractionToImproper([3, 1, 2, 4]))
+def isPermutation(n, inputArray):
+    if len(inputArray) <=1: return False;
+
+    if checkSequense(inputArray): return True;
+
+    for i in range(0, n-1):
+        if inputArray[i] > inputArray[i + 1]:
+            tmpArray = inputArray.copy()
+            tmpArray[i] = inputArray[i+1]
+            tmpArray[i+1] = inputArray[i]
+            
+            if checkSequense(tmpArray): return True;
+            
+    return False;
+
+def checkSequense(inputArray):
+    for i in range(0,len(inputArray) - 1):
+        if inputArray[i] != inputArray[i+1] - 1:
+            return False;
+    else: return True;
+
+print(isPermutation(2, [2, 0]))
