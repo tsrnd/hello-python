466b70f13c3f91d4f355683a4050604354320af7
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2018-12-19 10:00:16 +0700

    learn number

---
+++ number.py
@@ -0,0 +1,14 @@
+import math
+
+number = -5.5
+print("abs : "+str(abs(number)))
+print("ceil : "+str(math.ceil(number)))
+print("fabs : "+str(math.fabs(number)))
+print("ceil : "+str(math.floor(number)))
+print("log : "+str(math.log(-number)))
+print("max : "+str(max(1,2,-4,5,4)))
+print("min : "+str(min(1,2,-4,5,4)))
+print("modf : "+str(math.modf(number)))
+print("pow : "+str(math.pow(5,2)))
+print("round : "+str(round(5.123456,3)))
+print("sqrt : "+str(math.sqrt(25)))
