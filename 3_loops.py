78614e3e38fc2f90bb07f6fcc521b6b91b14d89d
at-minhdao <minh.dao@asiantech.vn>
2018-12-19 10:14:59 +0700

    loops

---
+++ 3_loops.py
@@ -0,0 +1,14 @@
+for letter in 'Python':
+    print('Current Letter :', letter)
+
+days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+for day in days:
+    print('Current day :', day)
+
+for i in range(len(days)):
+    print('Current day :', days[i])
+
+i = 0
+while i < len(days):
+    print('Current day :', days[i])
+    i += 1
