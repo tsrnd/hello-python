9714cb70161a7d3604c99383406726279f854590
Tuan Mai A <tuan.mai@asiantech.vn>
2018-12-19 14:04:54 +0700

    - Tìm hiểu về tuple

---
+++ at-tuanmai2/tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# Giống list, nhưng khai báo trong giấu (), khi khai báo rồi thì không thay đổi được giá trị
+# không có các phương thức append, pop ...
+tp = (1, 2, 3, 4 ,5)
+print(tp)
+#truy xuất, tìm kiếm giống list
