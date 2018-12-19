f2dea478e77f3941cf95db2c103a9321032be29d
Dao Nguyen V <dao.nguyen@asiantech.vn>
2018-12-19 11:59:01 +0700

    basic operators

---
+++ basic_operators.py
@@ -0,0 +1,23 @@
+# Arithmetic Operators
+# Comparison (Relational) Operators
+# Assignment Operators
+# Logical Operators
+# Bitwise Operators
+# Membership Operators
+num = 1
+seq1 = {'1': 1}
+seq2 = {1: '1'}
+if num in seq1:
+  print('num is in seq1')
+else:
+  print('num is not in seq1')
+if num in seq2:
+  print('num is in seq2')
+else:
+  print('num is not in seq2')
+'''
+num is not in seq1
+num is in seq2
+'''
+
+# Identity Operators
