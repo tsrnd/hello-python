b51b31d48970718c1ddf3cedf17de2ea2a6e2407
at-minhdao <minh.dao@asiantech.vn>
2018-12-19 09:52:58 +0700

    if else

---
+++ 2_decision_making.py
@@ -0,0 +1,15 @@
+list_1 = [1, 2, 3]
+if 1 in list_1:
+    print("1 exist in list")
+else:
+    print("1 is not exist in list")
+
+a = 2
+if a < 1:
+    print("a < 1")
+elif a < 2:
+    print("a < 2")
+elif a < 3:
+    print("a < 3")
+else:
+    print("a >= 3")
