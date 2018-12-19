275eae51e4dea1f922a003c67167a09afa80c464
at-nale <na.le@asiantech..vn>
2018-12-19 14:33:24 +0700

    example list

--- list.py
+++ list.py
@@ -1,15 +1,34 @@
 mylist = [1, 2, 3]
 mylist1 = [2, 3, 4]
 aList = ['xyz', 'abc']
+aList[1] = 'axb'
+# Thay doi 1 gia tri cua list cu the
+print(aList)
+# Do dai cua mang
+print(len(aList))
+# Chen 1 muc lam vi tri thu 2
+aList.insert(1, 'sdad')
+print(aList)
+# Loai bo muc duoc chi dinh
+aList.remove('sdad')
+print(aList)
 # Lay phan tu thu 2 trong mylist
 print(mylist[1])
-# Lay tu phan tu dau den phan tu 2 trong mylist
-print(mylist[:2])
+# Lay tu phan tu dau den phan tu 2
+print(aList[:1])
 print("-----------------")
 # Xoa phan tu thu 1 cua mang
-del mylist[0]
-print(mylist)
+del aList[0]
+print(aList)
 print("--------------")
+# Xoa phan tu cuoi cung
+print(mylist.pop())
+print(mylist)
+# Xoa trong list
+print(mylist.clear())
+#
+thislist = list(("a", "b", "c"))
+print(thislist)
 # Cong 2 mang
 print(mylist + mylist1)
 print("----------------")
@@ -22,8 +41,8 @@ mynumber = mylist.pop()
 print(mynumber)
 print(mylist)
 print("-----------------")
-# Vi tri cua phan tu
-print("Index for xyz : ", aList.index('xyz'))
+# # Vi tri cua phan tu
+# print("Index for xyz : ", aList.index('xyz'))
 print("------------------")
 # Dao nguoc vi tri cua mang
 mylist.reverse()
@@ -35,6 +54,3 @@ print("List: ", aList)
 user = {'name': 'Jone', 'age': 23}
 user['country'] = 'Vietnam'
 print(user)
-print("------------------")
-value = user.values()
-print(value)
