d979809465bd2fa927af0fe104587fcbbfe6d984
at-nale <na.le@asiantech..vn>
2018-12-19 14:34:34 +0700

    example operator

---
+++ operator.py
@@ -0,0 +1,22 @@
+a = 5
+b = 10
+print(a * b)
+print(a + b)
+print(b - a)
+print(b / a)
+print(b % a)
+print(10//3)
+print(10//3.0)
+fruits = ["orange", "banana"]
+if "orange" in fruits:
+    print("orange is a fruit")
+if "suggar" not in fruits:
+    print("suggar is  not a fruit ")
+if a == b:
+    print("a and b is equal")
+if a != b:
+    print("a and b is not equal")
+if a == b or a < b:
+    print("At least one of the statements is true")
+if a == 5 and b == 10:
+    print("Both the statements is true")
