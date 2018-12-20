558ea69824b76193bdcc7163960971f863b1f18c
Nguyen Van Phuc <mba0123@MBA0123.local>
2018-12-20 10:15:30 +0700

    Learning funtion

---
+++ Function.py
@@ -0,0 +1,44 @@
+print("Hello, this is calculator")
+
+
+def plus(a, b):
+    return "{}+{}={}".format(a, b, int(a)+int(b))
+
+
+def tru(a, b):
+    return "{}-{}={}".format(a, b, int(a)-int(b))
+
+
+def nhan(a, b):
+    return "{}*{}={}".format(a, b, int(a)*int(b))
+
+
+def chia(a, b):
+    return "{}/{}={}".format(a, b, int(a)/int(b))
+
+
+def calculator():
+    a = input("Enter [interger] a: ")
+    b = input("Enter [interger] b: ")
+    operator = input("Enter operator: [+,-,*,/]: ")
+    try:
+        if operator == "+":
+            print(plus(a, b))
+        elif operator == "-":
+            print(tru(a, b))
+        elif operator == "*":
+            print(nhan(a, b))
+        elif operator == "/":
+            print(chia(a, b))
+        else:
+            print("Operator not correct, please check agian !")
+    except:
+        print("Error Syntax, please check agian !")
+    option = input("Do you want continues ? [Yes/No]: ")
+    if str(option).upper() == "yes".upper():
+        calculator()
+    else:
+        print("Good Bye")
+        return
+
+calculator()
