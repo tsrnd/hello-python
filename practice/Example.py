22932ac82e50b070c6cd31472625f4b3a126576d
Tanh Le D <tanh.le@asiantech.vn>
2018-12-20 14:09:06 +0700

    import os module

--- practice/Example.py
+++ practice/Example.py
@@ -1,3 +1,5 @@
+import os
+
 def invert_dict(d):
     inv = dict()
     for key in d:
@@ -9,3 +11,10 @@ def invert_dict(d):
     return inv
 print(invert_dict({'a': 1, 'b': 1, 'c': 2, 'd': 3, 'e': 2}))
 
+def checkIfSameDigit(num):
+   return len(set(str(num))) == 1
+print(checkIfSameDigit(1111))
+
+print('I have %d girls friend in %d years' %(3, 2))
+print(os.getcwd())
+print(os.path.abspath('output.txt'))
