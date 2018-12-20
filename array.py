6baec60aa90b4d84914b6e92b304a13f5dc497c1
phutran1 <justinphus@gmail.com>
2018-12-20 08:59:32 +0700

    update code

--- array.py
+++
@@ -1,8 +0,0 @@
-cars = ["Ford", "Volvo", "BMW"]
-# a = "phu"
-# [a]+cars
-cars.append("Honda")
-cars.insert(3,"Honda")
-cars.pop(3)
-
-print(cars)
--- day1&2.py
+++ day1&2.py
@@ -1,3 +1,16 @@
+#if else
+age = 15
+if age > 17: 
+  print("can see a rated R movie")
+elif age < 17 and age > 12:
+  print("can see a rated PG-13 movie")
+else: 
+  print("can only see rated PG movies")
+
+if age>12:
+      print("12")
+
+
 #enumerate
 presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson"]
 for i in range(len(presidents)):
@@ -20,8 +33,8 @@ for i, color in enumerate(colors):
     print("in i ", i)
     print("ratio", ratio)
     print("{}% {}".format(ratio * 100, color))
-
-zip
+    
+#zip
 colors = ["red", "green", "blue", "purple"]
 ratios = [0.2, 0.3, 0.1, 0.4]
 for color, ratio in zip(colors, ratios):
@@ -30,3 +43,47 @@ for color, ratio in zip(colors, ratios):
 alist = ['a1', 'a2', 'a3']
 for i, a in enumerate(alist):
     print(i, a)
+#item
+def capitalizeVowelsRegExp(input):
+    substitutions = {
+    'a' :  'A',
+    'e' :  'E',
+    'i' :  'I',
+    'o' :  'O',
+    'u' :  'U',
+    'y' :  'Y',
+  }
+for before, after in substitutions.items():
+    input =  input.replace(before, after)
+return input
+
+
+print(capitalizeVowelsRegExp("aasdsadasphu"))
+
+
+#while
+color = ["1","4","3"]
+i = 0
+while i < len(color):
+    print(color[i])
+    i += 1
+print(range(len(color)))
+print(len(color))
+
+print(range(3))
+for i in range(0,3):
+    print("range", i)
+    print(color[i])
+
+for i in color:
+    print(i)
+
+#array
+cars = ["Ford", "Volvo", "BMW"]
+a = "phu"
+[a]+cars
+cars.append("Honda")
+cars.insert(3,"Honda")
+cars.pop(3)
+
+print(cars)
