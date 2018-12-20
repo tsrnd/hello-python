ef765d37f09cda63318c6f3ec1ae42c303a0a28a
Son Vu L. L <son.vu@asiantech.vn>
2018-12-20 10:27:28 +0700

    operator and syntax day 2

---
+++ Day2/operator.py
@@ -0,0 +1,33 @@
+a = 5
+b = 10
+
+# Addition
+def addition():
+    return a + b
+
+# Subtraction
+def subtraction():
+    return a - b
+
+# Multiple
+def multiple():
+    return a * b
+
+# Division 
+def division():
+    return a / b
+
+# Modulus
+def modulus():
+    return a % b
+
+# Exponentiation
+def exponentiation():
+    return a ** b
+
+# Floor division	
+def floorDivision():
+    return a // b
+
+
+
---
+++ Day2/syntax.py
@@ -0,0 +1,45 @@
+def sum(a, b):
+    return a + b
+
+sum(4,5)
+sum("hello", "python")
+
+# Type 
+def typeIdentification(a):
+    return type(a)
+
+typeIdentification(1)
+typeIdentification(2.8)
+typeIdentification("2")
+typeIdentification(1j)
+
+# Casting
+def castStrToInt(str): 
+    try:
+        return int(str)
+    except:
+        print("can't not cast")
+
+# String
+def getStringByPosition(str):
+    # get string by index 2 -> 5
+    return str[2:5]
+
+getStringByPosition("0123456789")
+
+# Strip
+def stripString(str):
+    return str.strip()
+
+stripString("       Hello python   ") 
+
+# Get length of String
+def lengthString(str):
+    return len(str)
+
+print(lengthString("123455"))
+
+
+
+
+
