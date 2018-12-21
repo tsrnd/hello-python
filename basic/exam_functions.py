db0a16d69cfe4a8ab17bd39d32b1fb20b59b535b
Tuan Dang Q <tuan.dang@asiantech.com.vn>
2018-12-21 14:42:53 +0700

    Update test case for catch Exception and update source code for case appropriate.

--- basic/exam_functions.py
+++ basic/exam_functions.py
@@ -1,11 +1,19 @@
 # =============================== Example about Functions. ===============================
 def funcWithDefaultValue(national='Viet Nam'):
     print('i come from ' + national)
-    return;
+    return
 
 # recursion Function
+
+
 def recursionFactorial(n):
-    if n==0:
+
+    try:
+        check = int(n)
+    except Exception as e:
+        raise Exception('It is not digit.')
+
+    if n == 0:
         raise Exception('Can not calculate factorial for 0.')
 
     if n == 1:
@@ -13,19 +21,29 @@ def recursionFactorial(n):
     return n*recursionFactorial(n-1)
 
 # Function with more argument.
+
+
 def moreArguments(name, age, *moreAtt):
     print('Name :', name)
     print('Age :', age)
     for item in moreAtt:
         print('Addition info :', item)
-    return;
+    return
 
 # =============================== Example about Lambda. ===============================
 
 
 def quadratic_function(a, b, c):
+    try:
+        testA = int(a)
+        testB = int(b)
+        testC = int(c)
+    except Exception as e:
+        raise Exception('input number doesn\'t number. ')
     return lambda x: a*x**2 + b*x+c
 
 # Only Lambda.
+
+
 def myfunc(n):
     return lambda a: a * n
