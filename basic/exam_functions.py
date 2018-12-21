941e6421657f22a07882d2c44d1297bc0dd93a8b
Tuan Dang Q <tuan.dang@asiantech.com.vn>
2018-12-21 09:43:44 +0700

    The first Unit test.

--- basic/exam_functions.py
+++ basic/exam_functions.py
@@ -1,36 +1,21 @@
 # =============================== Example about Functions. ===============================
 def funcWithDefaultValue(national='Viet Nam'):
     print('i come from ' + national)
-
-
-str = input()
-if len(str) > 0:
-    funcWithDefaultValue(str)
-else:
-    funcWithDefaultValue()
+    return;
 
 # recursion Function
-
-
 def recursionFactorial(n):
     if n == 1:
         return 1
     return n*recursionFactorial(n-1)
 
-
-print(recursionFactorial(4))
-
 # Function with more argument.
-
-
 def moreArguments(name, age, *moreAtt):
     print('Name :', name)
     print('Age :', age)
     for item in moreAtt:
         print('Addition info :', item)
-
-
-moreArguments('Dai', 25, 'Python', 'Lead member', 'Married')
+    return;
 
 # =============================== Example about Lambda. ===============================
 
@@ -38,16 +23,6 @@ moreArguments('Dai', 25, 'Python', 'Lead member', 'Married')
 def quadratic_function(a, b, c):
     return lambda x: a*x**2 + b*x+c
 
-
-fx = quadratic_function(1, 2, 3)
-print(fx(2))
-
 # Only Lambda.
-
-
 def myfunc(n):
     return lambda a: a * n
-
-
-mydoubler = myfunc(5)
-print(mydoubler(11))
