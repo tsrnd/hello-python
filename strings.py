efaea20db3ec95aa8b3ce1eb69f65edb9ce0dba4
Dao Nguyen V <dao.nguyen@asiantech.vn>
2018-12-21 14:40:37 +0700

    strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+print('= Strings =')
+
+msg = 'Hello \n Python'
+print('string: ' + msg)
+msg = r'Hello \n Python'
+print('raw string: ' + msg)
+'''
+= Strings =
+string: Hello
+ Python
+raw string: Hello \n Python
+'''
+
