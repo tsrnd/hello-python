9f3aeac333b77178ad5af70c1cb2aa7c2dfc55bc
at-tamnguyen4 <tam.nguyen4@asiantech.vn>
2018-12-23 23:10:06 +0700

    fix ci

--- sorting/sort_test.py
+++ sorting/sort_test.py
@@ -4,21 +4,18 @@ from sorting_algorithm import gnomeSort, quickSort
 
 
 def randomArray():
-    array = []
-    for _ in range(10000):
-        array.append(random.randint(-100000, 100000))
-    return array
+    return [random.randint(-100000, 100000) for _ in range(10000)]
 
 
-# class MyTestSort(unittest.TestCase):
-#     def testQuickSort(self):
-#         for _ in range(1):
-#             array = randomArray()
-#             sorted_list = sorted(array)
-#             self.assertEqual(sorted_list, quickSort(array))
+class MyTestSort(unittest.TestCase):
+    def testQuickSort(self):
+        for _ in range(100):
+            array = randomArray()
+            sorted_list = sorted(array)
+            self.assertEqual(sorted_list, quickSort(array))
 
-#     def testGnomeSort(self):
-#         for _ in range(1):
-#             array = randomArray()
-#             sorted_list = sorted(array)
-#             self.assertEqual(sorted_list, gnomeSort(array))
+    def testGnomeSort(self):
+        for _ in range(100):
+            array = randomArray()
+            sorted_list = sorted(array)
+            self.assertEqual(sorted_list, gnomeSort(array))
