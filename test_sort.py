27f3934ea038a9a589b39137ef499c01474bb95f
Son Vu L. L <son.vu@asiantech.vn>
2018-12-24 00:47:17 +0700

    change function test

--- test_sort.py
+++ test_sort.py
@@ -1,22 +1,20 @@
 import unittest, random
 from sort import *
 
+
 class Testing(unittest.TestCase):
-    max_test_case = 5
+    maxDiff = None
+    max_test_case = 100
     min_range = -100000
     max_range = 100000
     total_number = 10000
 
-    # def testBubleSort(self):
+    # def selectionSort(self):
     #     for _ in range(self.max_test_case):
     #         case = [random.randint(self.min_range, self.max_range) for _ in range(self.total_number)]
-    #         case_copy = case.copy()
-    #         sorted(case_copy)
-    #         self.assertEqual(case_copy, bubleSort(case))
+    #         self.assertEqual(sorted(case), selectionSort(case))
 
-    def testSelectionSort(self):
+    def testBubleSort(self):
         for _ in range(self.max_test_case):
-            case = [random.randint(self.min_range, self.max_range) for _ in range(self.total_number)]
-            case_copy = case.copy()
-            sorted(case_copy)
-            self.assertEqual(case_copy, selectionSort(case))
+            case = [random.randrange(self.min_range, self.max_range) for _ in range(self.total_number)]
+            self.assertEqual(sorted(case), bubleSort(case))
