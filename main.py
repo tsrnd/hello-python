1b0716e82309ecd60b1667aba5abb2686ba4c601
phutran1 <justinphus@gmail.com>
2018-12-24 15:16:49 +0700

    remove code

--- main.py
+++ main.py
@@ -1,38 +1,3 @@
-###quick sort
-def quickSort(array):
-    less = []
-    equal = []
-    greater = []
-
-    if len(array) > 1:
-        pivot = array[0]
-        for x in array:
-            if x < pivot:
-                less.append(x)
-            if x == pivot:
-                equal.append(x)
-            if x > pivot:
-                greater.append(x)
-        return quickSort(less)+equal+quickSort(greater)
-    else:
-        return array
-
-
-###shell sort
-def shellSort(arr): 
-    n = len(arr) 
-    gap = n//2
-    while gap > 0: 
-        for i in range(gap,n): 
-            temp = arr[i] 
-            j = i 
-            while  j >= gap and arr[j-gap] >temp: 
-                arr[j] = arr[j-gap] 
-                j -= gap 
-            arr[j] = temp 
-        gap //= 2
-    return arr
-
 ###bubble sort
 def bubbleSort(arr):
     for passnum in range(len(arr)-1,0,-1):
