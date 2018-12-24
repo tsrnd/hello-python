375dc402bcdae616ba5f687b0690df3422a2f0c1
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2018-12-24 13:34:01 +0700

    change way add list

--- test.py
+++ test.py
@@ -6,18 +6,14 @@ from quick_sort import quickSort
 class Testing(unittest.TestCase):
     def testQuickSort(self):
         for _ in range(100):
-            list_sort = []
-            for _ in range(10000):
-                list_sort.append(random.randint(-100000, 100000))
+            list_sort = [random.randint(-100000, 100000) for _ in range(10000)]
             list_copy = list_sort.copy()
             list_copy.sort()
             self.assertEqual(list_copy, quickSort(list_sort))
 
     def testShellSort(self):
         for _ in range(100):
-            list_sort = []
-            for _ in range(10000):
-                list_sort.append(random.randint(-100000, 100000))
+            list_sort = [random.randint(-100000, 100000) for _ in range(10000)]
             list_copy = list_sort.copy()
             list_copy.sort()
             self.assertEqual(list_copy, shellSort(list_sort))
