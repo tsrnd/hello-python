4e4faf7c0a592cb92e18cd3d304d07876a1d8b2d
Hoa Hoang T <hoa.hoang@asiantech.vn>
2018-12-24 16:54:23 +0700

    update test case sorting

--- test/test_sorting.py
+++ test/test_sorting.py
@@ -1,21 +1,33 @@
 from sorting import *
 import random
+import pytest
 
 
-def randomInputArray():
+def random_input_array():
     array = []
     for _ in range(9999):
         array.append(random.randint(-100000, 100000))
     return array
 
 
-def test_quick_sort():
-    a = randomInputArray()
-    b = sorted(a)
-    assert b == quickSort(a)
+def create_input_test():
+    result = []
+    for _ in range(50):
+        a = random_input_array()
+        result.append(a)
+    return result
 
 
-def test_heap_sort():
-    a = randomInputArray()
-    b = sorted(a)
-    assert b == heapSort(a)
+@pytest.fixture(params=create_input_test())
+def data(request):
+    return request.param
+
+
+def test_quick_sort(data):
+    b = sorted(data)
+    assert b == quickSort(data)
+
+
+def test_heap_sort(data):
+    b = sorted(data)
+    assert b == heapSort(data)
