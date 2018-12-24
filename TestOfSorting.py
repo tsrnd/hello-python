53c80d8ff8ed1abd9f391a32eea7b6d9c6c28c12
Nguyen Van Phuc <mba0123@MBA0123.local>
2018-12-24 14:41:17 +0700

    update test

--- TestOfSorting.py
+++ TestOfSorting.py
@@ -3,32 +3,18 @@ import unittest
 from random import shuffle, randint
 
 
-def initListTest(testCases, bound=10_000, rangeStart=-100_000, rangeEnd=100_000):
-    listTestCase = []
-    for _ in range(testCases):
-        temp = []
-        for _ in range(bound):
-            temp.append(randint(rangeStart, rangeEnd))
-        listTestCase.append(temp)
-    return listTestCase
+def initListTest(bound=10_000, rangeStart=-100_000, rangeEnd=100_000):
+    return [randint(rangeStart, rangeEnd) for _ in range(bound)]
 
 
 class TestOfSorting(unittest.TestCase):
     def testMyInsertSort(seft):
-        numOfTestCase = 100
-        listTest = initListTest(numOfTestCase)
-        for i in range(numOfTestCase):
-            print("Progress of testMyInsertSort: {} / {} %".format(i+1, numOfTestCase))
-            seft.assertEqual(MySorting.insertSort(
-                listTest[i]), sorted(listTest[i]))
+        listTest = initListTest()
+        seft.assertEqual(MySorting.insertSort(listTest), sorted(listTest))
 
-    def testMyBubbleSort(seft):
-        numOfTestCase = 100
-        listTest = initListTest(numOfTestCase)
-        for i in range(numOfTestCase):
-            print("Progress of testMyBubbleSort: {} / {} %".format(i+1, numOfTestCase))
-            seft.assertEqual(MySorting.bubleSort(
-                listTest[i]), sorted(listTest[i]))
+    def testMyInsertSort(seft):
+        listTest = initListTest()
+        seft.assertEqual(MySorting.bubleSort(listTest), sorted(listTest))
 
 
 if __name__ == "__main__":
