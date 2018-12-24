61964881018cfa025aa7edb1add7145ae613bd42
at-anhngo <anh.ngo@asiantech.vn>
2018-12-24 16:13:09 +0700

    moddified unittest to pytest style

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,24 +1,23 @@
-import unittest, random
+import random
 from main import quickSort, shellSort
 
-class Testing(unittest.TestCase):
-    const_round = 100
-    const_min_random = -100000
-    const_max_random = 100000
-    const_limit_case = 10000
+const_round = 100
+const_min_random = -100000
+const_max_random = 100000
+const_limit_case = 10000
 
-    #test quicksort with 100 list(10000)
-    def testQuickSort(self):
-        for _ in range(self.const_round):
-            case = [random.randint(self.const_min_random, self.const_max_random) for _ in range(self.const_limit_case)]
-            case_copy = case.copy()
-            case_copy.sort()
-            self.assertEqual(case_copy, quickSort(case))
+#test quicksort with 100 list(10000)
+def testQuickSort():
+    for _ in range(const_round):
+        case = [random.randint(const_min_random, const_max_random) for _ in range(const_limit_case)]
+        case_copy = case.copy()
+        case_copy.sort()
+        assert case_copy, quickSort(case)
 
-    #test shellsort with 100 list(10000)
-    def testShellSort(self):
-        for _ in range(self.const_round):
-            case = [random.randint(self.const_min_random, self.const_max_random) for _ in range(self.const_limit_case)]
-            case_copy = case.copy()
-            case_copy.sort()
-            self.assertEqual(case_copy, shellSort(case))
+#test shellsort with 100 list(10000)
+def testShellSort():
+    for _ in range(const_round):
+        case = [random.randint(const_min_random, const_max_random) for _ in range(const_limit_case)]
+        case_copy = case.copy()
+        case_copy.sort()
+        assert case_copy, shellSort(case)
