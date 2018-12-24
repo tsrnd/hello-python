e254a8f8d1567db36bdc135d4c12f7627e016e9e
Dao Nguyen V <dao.nguyen@asiantech.vn>
2018-12-24 14:35:37 +0700

    date time

--- date_time.py
+++ date_time.py
@@ -1,5 +1,27 @@
-from time import time
+print('= Date Time =')
 
-ticks = time()
-print('ticks = ' + str(ticks), 1)
+import time
+import calendar
 
+now = time.localtime()
+print('now (local) =', time.asctime(now), time.tzname[1])
+now = time.gmtime()
+print('now (utc) =', time.asctime(now))
+'''
+now (local) = Mon Dec 24 14:34:56 2018 +07
+now (utc) = Mon Dec 24 07:34:56 2018
+'''
+
+cal = calendar.month(2018, 12)
+print('calendar =\n', cal)
+'''
+calendar =
+    December 2018
+Mo Tu We Th Fr Sa Su
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 22 23
+24 25 26 27 28 29 30
+31
+'''
