a773bf5fcaa97afe38a325671d53437534e88bfd
at-thaile <thai.le@asiantech.vn>
2018-12-24 15:59:24 +0700

    change

--- test/main.py
+++ test/main.py
@@ -1,23 +1,23 @@
-def partition(arr,low,high): 
+def divide(data,low,high): 
     i = ( low-1 )        
-    pivot = arr[high]     
+    pivot = data[high]     
   
     for j in range(low , high): 
   
-        if   arr[j] <= pivot: 
+        if   data[j] <= pivot: 
             i = i+1 
-            arr[i],arr[j] = arr[j],arr[i] 
+            data[i],data[j] = data[j],data[i] 
   
-    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    data[i+1],data[high] = data[high],data[i+1] 
     return ( i+1 ) 
   
   
-def quickSort(arr,low,high): 
+def quickSort(data,low,high): 
     if low < high: 
-      pi = partition(arr,low,high) 
-      quickSort(arr, low, pi-1) 
-      quickSort(arr, pi+1, high)
-    return arr
+      i = divide(data,low,high) 
+      quickSort(data, low, i-1) 
+      quickSort(data, i+1, high)
+    return data
 
 def selectionSort(data, size):
   for i in range(0, size-1):
@@ -33,9 +33,9 @@ def selectionSort(data, size):
 
 
 def heapify(arr, n, i): 
-    largest = i # Initialize largest as root 
-    l = 2 * i + 1     # left = 2*i + 1 
-    r = 2 * i + 2     # right = 2*i + 2 
+    largest = i
+    l = 2 * i + 1  
+    r = 2 * i + 2 
   
     if l < n and arr[i] < arr[l]: 
         largest = l 
@@ -44,7 +44,7 @@ def heapify(arr, n, i):
         largest = r 
   
     if largest != i: 
-        arr[i],arr[largest] = arr[largest],arr[i] # swap 
+        arr[i],arr[largest] = arr[largest],arr[i] 
         heapify(arr, n, largest) 
         
 def heapSort(arr): 
@@ -52,6 +52,6 @@ def heapSort(arr):
   for i in range(n, -1, -1): 
       heapify(arr, n, i) 
   for i in range(n-1, 0, -1): 
-      arr[i], arr[0] = arr[0], arr[i] # swap 
+      arr[i], arr[0] = arr[0], arr[i]
       heapify(arr, i, 0) 
   return arr
