17d08da6545a016f3bc249480dfd2880cf2d7911
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2018-12-24 13:48:12 +0700

    add name value common

--- test.py
+++ test.py
@@ -4,16 +4,21 @@ from quick_sort import quickSort
 
 
 class Testing(unittest.TestCase):
+    max_num = 100000
+    min_num = -100000
+    total_case = 100
+    range_array = 10000
+
     def testQuickSort(self):
-        for _ in range(100):
-            list_sort = [random.randint(-100000, 100000) for _ in range(10000)]
+        for _ in range(self.total_case):
+            list_sort = [random.randint(self.min_num, self.max_num) for _ in range(self.range_array)]
             list_copy = list_sort.copy()
             list_copy.sort()
             self.assertEqual(list_copy, quickSort(list_sort))
 
     def testShellSort(self):
-        for _ in range(100):
-            list_sort = [random.randint(-100000, 100000) for _ in range(10000)]
+        for _ in range(self.total_case):
+            list_sort = [random.randint(self.min_num, self.max_num) for _ in range(self.range_array)]
             list_copy = list_sort.copy()
             list_copy.sort()
             self.assertEqual(list_copy, shellSort(list_sort))
