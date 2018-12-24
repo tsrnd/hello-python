0a5d3a4e32c054786e3711dfa9858be51925041f
Nguyen Van Phuc <mba0123@MBA0123.local>
2018-12-24 12:25:42 +0700

    add more test case

--- TestOfSorting.py
+++ TestOfSorting.py
@@ -1,28 +1,34 @@
 import Sorting as MySorting
 import unittest
-from random import shuffle
+from random import shuffle, randint
+
+
+def initListTest(testCases, bound=10_000, rangeStart=-100_000, rangeEnd=100_000):
+    listTestCase = []
+    for _ in range(testCases):
+        temp = []
+        for _ in range(bound):
+            temp.append(randint(rangeStart, rangeEnd))
+        listTestCase.append(temp)
+    return listTestCase
 
 
 class TestOfSorting(unittest.TestCase):
     def testMyInsertSort(seft):
-        limit = 10_000
-        expecta = []
-        actual = []
-        for i in range(limit):
-            expecta.append(i)
-            actual.append(i)
-        shuffle(actual)
-        seft.assertEqual(MySorting.insertSort(actual), expecta)
+        numOfTestCase = 100
+        listTest = initListTest(numOfTestCase)
+        for i in range(numOfTestCase):
+            print("Progress of testMyInsertSort: {} / {} %".format(i+1,numOfTestCase))
+            seft.assertEqual(MySorting.insertSort(
+                listTest[i]), sorted(listTest[i]))
 
     def testMyBubbleSort(seft):
-        limit = 10_000
-        expecta = []
-        actual = []
-        for i in range(limit):
-            expecta.append(i)
-            actual.append(i)
-        shuffle(actual)
-        seft.assertEqual(MySorting.bubleSort(actual), expecta)
+        numOfTestCase = 100
+        listTest = initListTest(numOfTestCase)
+        for i in range(numOfTestCase):
+            print("Progress of testMyBubbleSort: {} / {} %".format(i+1),numOfTestCase)
+            seft.assertEqual(MySorting.bubleSort(
+                listTest[i]), sorted(listTest[i]))
 
 
 if __name__ == "__main__":
