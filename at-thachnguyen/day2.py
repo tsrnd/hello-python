7921a709eb34436d8bef00d219babfda35c36aa3
at-thachnguyen <thach.nguyen@asiantech.vn>
2018-12-24 09:46:58 +0700

    remove import

--- at-thachnguyen/day2.py
+++ at-thachnguyen/day2.py
@@ -9,12 +9,12 @@ list_example = list(range(1,10))
 for i in list_example[2:5]:
   print(i)
 #-----------------
-import time;
-now = time.time()
-print(now)
-print(time.localtime())
+# import time;
+# now = time.time()
+# print(now)
+# print(time.localtime())
 
-import datetime
-today = datetime.date.today()
-print(today.strftime('Today is %d, %b %Y'))
-print('Today is ' + today.strftime('%A'))
+# import datetime
+# today = datetime.date.today()
+# print(today.strftime('Today is %d, %b %Y'))
+# print('Today is ' + today.strftime('%A'))
