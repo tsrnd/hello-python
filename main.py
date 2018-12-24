64dd22a9631d6b7382004fd2662568d14e133097
phutran1 <justinphus@gmail.com>
2018-12-24 10:20:41 +0700

    edit insertion sort

--- main.py
+++ main.py
@@ -45,16 +45,15 @@ def bubbleSort(arr):
 
 ###insertion sort
 def insertionSort(arr):
-    a = [3, 6, 9, 2, 6, 8, 9, 1, 7, 21, 7, 12]
-    for i in range(1, len(a)):
-        t = a[i]
+    for i in range(1, len(arr)):
+        t = arr[i]
         index = i
         for j in range(i -1, -1, -1):  
-            if a[j] > t:
-                a[j + 1] = a[j]
+            if arr[j] > t:
+                arr[j + 1] = arr[j]
                 index = j
             else:
                 break
         if index != i:
-            a[index] = t
+            arr[index] = t
     return arr
