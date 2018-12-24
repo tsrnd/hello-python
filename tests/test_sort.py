9addd0026399ed04f69bbd711baaa08a94638167
phutran1 <justinphus@gmail.com>
2018-12-24 17:01:55 +0700

    remove class to fit pytest

--- tests/test_sort.py
+++ tests/test_sort.py
@@ -1,24 +1,23 @@
-import unittest, random
+import random
 from main import bubbleSort, insertionSort
 
-class Testing(unittest.TestCase):
-    const_round = 100
-    const_min_random = -100000
-    const_max_random = 100000
-    const_limit_case = 100
-    #test bubblesort with 100 list(10000)
-    def testBubbleSort(self):
-        for _ in range(self.const_round):
-            case = [random.randint(self.const_min_random, self.const_max_random) for _ in range(self.const_limit_case)]
-            case_copy = case.copy()
-            case_copy.sort()
-            self.assertEqual(case_copy, bubbleSort(case))
+const_round = 100
+const_min_random = -100000
+const_max_random = 100000
+const_limit_case = 100
 
-    #test insertion  with 100 list(10000)
-    def testInsertionSort(self):
-        for _ in range(self.const_round):
-            case = [random.randint(self.const_min_random, self.const_max_random) for _ in range(self.const_limit_case)]
-            case_copy = case.copy()
-            case_copy.sort()
-            self.assertEqual(case_copy, insertionSort(case))
+#test bubblesort with 100 list(10000)
+def testBubbleSort():
+    for _ in range(const_round):
+        case = [random.randint(const_min_random, const_max_random) for _ in range(const_limit_case)]
+        case_copy = case.copy()
+        case_copy.sort()
+        assert case_copy, bubbleSort(case)
 
+#test insertion  with 100 list(10000)
+def testInsertionSort():
+    for _ in range(const_round):
+        case = [random.randint(const_min_random, const_max_random) for _ in range(const_limit_case)]
+        case_copy = case.copy()
+        case_copy.sort()
+        assert case_copy, insertionSort(case)
