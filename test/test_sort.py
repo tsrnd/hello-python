29d03f796b5cc785d14ee9f367244b4cc8a954d2
at-thaile <thai.le@asiantech.vn>
2018-12-24 16:55:45 +0700

    change

--- test/test_sort.py
+++ test/test_sort.py
@@ -9,20 +9,20 @@ def Rand(start, end, num):
 
  
   
-class testQuickSort(unittest.TestCase):
+class testSort(unittest.TestCase):
     def test_quick_sort(self):
         for _ in range(100):
             array = Rand(-100000, 100000, 10000)
             arraysort = array.copy()
             arraysort.sort()
             self.assertEqual(quickSort(array,0,10000-1), arraysort)
- 
-  
-class TestHeapSort(unittest.TestCase):
+
     def test_heap_sort(self):
         for _ in range(100):
             array = Rand(-100000, 100000, 10000)
             arraysort = array[:]
             arraysort.sort()
-            self.assertEqual(heapSort(array), arraysort)
+            self.assertEqual(heapSort(array), arraysort)    
+
+
 
