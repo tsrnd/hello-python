85e1137876a52e17bcc1255cf6981f435801e7b6
Nghia Vo V <nghia.vo@asiantech.vn>
2018-12-25 14:29:28 +0700

    network

---
+++ network.py
@@ -0,0 +1,18 @@
+# The Program connects to a web server
+import socket
+import time
+
+HOST = 'data.pr4e.org'
+PORT = 80
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect((HOST, PORT))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+mysock.close()
