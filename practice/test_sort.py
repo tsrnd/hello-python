a2bae1b838cdd1a61610b89980461e4ea1ed9ca3
Tanh Le D <tanh.le@asiantech.vn>
2018-12-25 17:23:44 +0700

    Remove '/' character

--- practice/test_sort.py
+++ practice/test_sort.py
@@ -3,9 +3,9 @@ import unittest
 
 
 def mergeSort(arr):
-    if len(arr) >1: 
-        mid = len(arr)//2 
-        L = arr[:mid] 
+    if len(arr) > 1:
+        mid = len(arr)/2
+        L = arr[:mid]
         R = arr[mid:]
 
         mergeSort(L)
@@ -13,24 +13,24 @@ def mergeSort(arr):
 
         i = j = k = 0
 
-        while i < len(L) and j < len(R): 
-            if L[i] < R[j]: 
-                arr[k] = L[i] 
-                i+=1
-            else: 
-                arr[k] = R[j] 
-                j+=1
-            k+=1
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
 
-        while i < len(L): 
-            arr[k] = L[i] 
-            i+=1
-            k+=1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
 
-        while j < len(R): 
-            arr[k] = R[j] 
-            j+=1
-            k+=1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
 
     return arr
 
