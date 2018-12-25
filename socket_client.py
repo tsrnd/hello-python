48c214f790ba6b68ed79732d79546ae0ec484d0b
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2018-12-25 09:53:56 +0700

    learn socket

---
+++ socket_client.py
@@ -0,0 +1,10 @@
+import socket
+
+HOST = 'localhost'    # Cấu hình address server
+PORT = 8000              # Cấu hình Port sử dụng
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cấu hình socket
+s.connect((HOST, PORT)) # tiến hành kết nối đến server
+s.sendall(b'Hello server!') # Gửi dữ liệu lên server 
+data = s.recv(1024) # Đọc dữ liệu server trả về
+print('Server Respone: ', repr(data))
+s.close()
