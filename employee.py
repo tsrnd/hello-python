f6c50e2070ea73e560839bde16343843714444a4
at-nale <na.le@asiantech.vn>
2018-12-26 10:23:02 +0700

    add test

--- employee.py
+++ employee.py
@@ -5,7 +5,6 @@ class Employee():
         self.address = address
 
     def displayEmployee(self):
-        # return "Name:{},Age:{},Address:{}.".format(self.name, self.age, self.address)
         return ("Name:%s, Age:%s,Address:%s" % (self.name, self.age, self.address))
 
     def setSalary(self, salary):
@@ -24,13 +23,13 @@ class Manager(Employee):
         return("name is %s and job is %s" % (self.name, self.job))
 
 
-em1 = Employee("Na", 23, "Da Nang")
-em2 = Employee("Tram", 34, "Quang Nam")
-print(em1.displayEmployee())
-print(em2.displayEmployee())
+# em1 = Employee("Na", 23, "Da Nang")
+# em2 = Employee("Tram", 34, "Quang Nam")
+# print(em1.displayEmployee())
+# print(em2.displayEmployee())
 
-em = Manager("Tam", 23, "Danang", "manager")
-print(em.setSalary(300))
-print(em.getSalary())
-print(em.displayEmployee())
-print(em.displayManager())
+# em = Manager("Tam", 23, "Danang", "manager")
+# print(em.setSalary(300))
+# print(em.getSalary())
+# print(em.displayEmployee())
+# print(em.displayManager())
--- employee_test.py
+++ employee_test.py
@@ -19,3 +19,30 @@ class TestEmployee(object):
         em = Employee(name, age, address)
         assert em.displayEmployee() == ("Name:%s, Age:%s,Address:%s" %
                                         (name, age, address))
+
+    @pytest.mark.parametrize("salary", [200])
+    def test_get_salary(self, salary):
+        em = Employee("test", 23, "test")
+        em.setSalary(salary)
+        get_salary = em.getSalary()
+        assert get_salary == ("Salary:", salary)
+
+
+class TestManager(object):
+    @pytest.mark.parametrize(
+        "name, age, address, job", [("Na", 23, "DN", "manager")]
+    )
+    def test_init_manager(self, name, age, address, job):
+        mana = Manager(name, age, address, job)
+        assert mana.name == name
+        assert mana.age == age
+        assert mana.address == address
+        assert mana.job == job
+
+    @pytest.mark.parametrize(
+        "name, age, address, job", [("Na", 23, "DN", "manager")]
+    )
+    def test_display_manager(self, name, age, address, job):
+        mana = Manager(name, age, address, job)
+        assert mana.displayManager() == (
+            "name is %s and job is %s" % (name, job))
