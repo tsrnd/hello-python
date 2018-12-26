c8dd9c1aee05a140483452a9ff6d831120efaaa8
at-trampham <tram.pham@asiantech.vn>
2018-12-26 10:04:14 +0700

    add tesst

--- test_class.py
+++ test_class.py
@@ -7,10 +7,49 @@ def test_my_class():
     assert ins.myfunc() == "My age is 23 and my name Tram"
 
 
-class TestInfo(object):
+class TestInfo:
     @pytest.mark.parametrize("address,name,age", [("QN", "Tram", 23)])
     def test_init(self, address, name, age):
         info = Info(address, name, age)
-        assert info.address == address
+        assert info.add == address
         assert info.age == age
         assert info.name == name
+
+    @pytest.mark.parametrize("address,name,age", [("QN", "Tram", 23)])
+    def test_display_info(self, address, name, age):
+        info = Info(address, name, age)
+        exp = ("Your address is %s and your name is %s and your age is %d" %
+               (address, name, age))
+        assert info.display_info() == exp
+
+
+class TestStudent:
+    @pytest.mark.parametrize(
+        "address,name,age,year", [("QN", "Tram", 23, "2018")]
+    )
+    def test_init(self, address, name, age, year):
+        student = Student(address, name, age, year)
+        assert student.add == address
+        assert student.name == name
+        assert student.age == age
+        assert student.year == year
+
+    @pytest.mark.parametrize(
+        "address,name,age,year", [("QN", "Tram", 23, "2018")]
+    )
+    def test_get_detail(self, address, name, age, year):
+        student = Student(address, name, age, year)
+        expect = ("Your name is %s and %d years old and is in %s year" %
+                  (name, age, year))
+        assert student.get_detail() == expect
+
+
+class TestMultiple:
+    @pytest.mark.parametrize(
+        "add,name,age", [("QN", "Tram", 23)]
+    )
+    def test_init(self, add, name, age):
+        multi = Multiple(add, name, age)
+        assert multi.add == add
+        assert multi.name == name
+        assert multi.age == age
