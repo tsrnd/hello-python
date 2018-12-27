0fd9031f0d693c13a26c879d19ff098362e3dc64
at-tamnguyen4 <tam.nguyen4@asiantech.vn>
2018-12-27 16:14:15 +0700

    fix test

--- sorting/sort_test.py
+++ sorting/sort_test.py
@@ -3,19 +3,15 @@ import unittest
 from sorting_algorithm import countingSort, quickSort
 
 
-def randomArray():
-    return [random.randint(-100000, 100000) for _ in range(10000)]
-
-
 class MyTestSort(unittest.TestCase):
     def testQuickSort(self):
         for _ in range(100):
-            array = randomArray()
+            array = [random.randint(-100000, 100000) for _ in range(10000)]
             sorted_list = sorted(array)
             self.assertEqual(sorted_list, quickSort(array))
 
     def testCountingSort(self):
         for _ in range(100):
-            array = randomArray()
+            array = [random.randint(-100000, 100000) for _ in range(10000)]
             sorted_list = sorted(array)
             self.assertEqual(sorted_list, countingSort(array))
